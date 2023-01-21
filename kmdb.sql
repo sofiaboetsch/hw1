6a8f8a18f53a6294f9ce77056be7af432ac8bf3b
sofiaboetsch <sofia.boetsch@kellogg.northwestern.edu>
2023-01-21 19:02:49 +0000

    first

--- kmdb.sql
+++ kmdb.sql
@@ -98,16 +98,78 @@
 -- Turns column mode on but headers off
 .mode column
 .headers off
+.width 20 20 20 20
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS characters;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE studios(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  studio_name TEXT
+);
+
+CREATE TABLE movies(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released INTEGER,
+  mpaa_rating TEXT,
+  studio_id TEXT
+);
+
+CREATE TABLE actors(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  actor_name TEXT
+);
+
+CREATE TABLE characters(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  characters_name TEXT, 
+  actor_id INTEGER,
+  movie_id INTEGER
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO studios (studio_name) VALUES ("Warner Bros."); 
+
+INSERT INTO movies (title, year_released, mpaa_rating, studio_id) VALUES ("Batman Begins", "2003", "PG-13",1);
+INSERT INTO movies (title, year_released, mpaa_rating, studio_id) VALUES ("The Dark Knigh", "2008", "PG-13",1);
+INSERT INTO movies (title, year_released, mpaa_rating, studio_id) VALUES ("The Dark Knigh Rises", "2012", "PG-13",1);
+
+INSERT INTO actors (actor_name) VALUES ("Christian Bale"),("Michael Caine"),("Liam Neeson"), ("Katie Holmes"),("Gary Oldman"), 
+("Heath Ledger"), ("Aaron Eckhart"), ("Maggie Gyllenhaal"), ("Tom Hardy"), ("Anne Hathaway"),("Joseph Gordon-Levitt");
+
+INSERT INTO characters (characters_name, actor_id, movie_id) VALUES ("Bruce Wayne", 1 , 1); 
+INSERT INTO characters (characters_name, actor_id, movie_id) VALUES ("Alfred",2 , 1);
+INSERT INTO characters (characters_name, actor_id, movie_id) VALUES ("Ra's Al Ghul",3 , 1);
+INSERT INTO characters (characters_name, actor_id, movie_id) VALUES ("Rachel Dawes", 4, 1);
+INSERT INTO characters (characters_name, actor_id, movie_id) VALUES ("Commissioner Gordon", 5, 1);
+
+
+
+--ifkwokfowekpfewk 
+-- Batman Begins          Michael Caine         Alfred
+-- Batman Begins          Liam Neeson           Ra's Al Ghul
+-- Batman Begins          Katie Holmes          Rachel Dawes
+-- Batman Begins          Gary Oldman           Commissioner Gordon
+-- The Dark Knight        Christian Bale        Bruce Wayne
+-- The Dark Knight        Heath Ledger          Joker
+-- The Dark Knight        Aaron Eckhart         Harvey Dent
+-- The Dark Knight        Michael Caine         Alfred
+-- The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
+-- The Dark Knight Rises  Christian Bale        Bruce Wayne
+-- The Dark Knight Rises  Gary Oldman           Commissioner Gordon
+-- The Dark Knight Rises  Tom Hardy             Bane
+-- The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
+-- The Dark Knight Rises  Anne Hathaway         Selina Kyle
+
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -116,6 +178,8 @@
 
 -- The SQL statement for the movies output
 -- TODO!
+SELECT movies.title, movies.year_released, movies.mpaa_rating, studios.studio_name FROM movies 
+INNER JOIN studios ON movies.studio_id = studios.id ;
 
 -- Prints a header for the cast output
 .print ""
